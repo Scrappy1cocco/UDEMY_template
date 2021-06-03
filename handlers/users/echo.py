f5850c0c90be8a3cd9aa5a43eb35cf3b07a546b5
Scrappy1cocco <jedediah.shine100@gmail.com>
2021-06-03 17:44:34 +0300

    сделал хендлер эхо -!

---
+++ handlers/users/echo.py
@@ -0,0 +1,16 @@
+from loader import dp, bot
+from aiogram import types
+
+
+@dp.message_handler(text="/start")
+async def bot_start(message: types.Message):
+    chat_id = message.chat.id
+    text = message.text
+    await bot.send_message(chat_id=chat_id, text=text)
+
+@dp.message_handler(content_types="text", state=None)
+async def bot_echo(message: types.Message):
+    text = message.text
+    # chat_id = message.chat.id
+    # await bot.send_message(chat_id=chat_id, text=text)
+    await message.reply(text)
